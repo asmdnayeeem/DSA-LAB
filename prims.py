a059f528857b7152824d09c36b47beca60694f5f
MAHAMMED NAYEEM AKBARSAGARI <85838699+asmdnayeeem@users.noreply.github.com>
2023-03-20 17:09:15 +0530

    final changes

--- prims.py
+++ prims.py
@@ -2,9 +2,7 @@ def prim(graph, start):
     visited = set()
     msc = 0
     mse = []
-
     visited.add(start)
-
     while len(visited) < len(graph):
         mw = float('inf')
         me = None
@@ -13,14 +11,11 @@ def prim(graph, start):
                 if v not in visited and weight < mw:
                     mw = weight
                     me = (u, v, weight)
-
         if me is None:
             break
-
         visited.add(me[1])
         mse.append(me)
         msc += mw
-
     return mse, msc
 
 
@@ -28,4 +23,4 @@ graph = {"A": [("B", 1), ("C", 3)], "B": [("A", 1), ("C", 2)], "C": [
     ("A", 3), ("B", 2), ("D", 5), ("E", 4)], "D": [("C", 5), ("E", 6)], "E": [("C", 4), ("D", 6)]}
 r = prim(graph, 'A')
 print(r)
-print(len(graph))
+
