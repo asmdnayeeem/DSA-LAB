568bc6b4b9894ef0bd3bc3597b20aeb0544c3c32
MAHAMMED NAYEEM AKBARSAGARI <85838699+asmdnayeeem@users.noreply.github.com>
2023-03-27 16:13:59 +0530

    lab completed

--- dijktras.py
+++ dijktras.py
@@ -9,7 +9,10 @@ def dijktras(graph, node):
         for j, k in graph[i]:
             if d[j] > d[i]+k:
                 d[j] = d[i]+k
+    for i in d:
+        print(node,"-->",i,':',d[i])
     return d
+    
 graph = {"A": [("B", 1), ("C", 3)], "B": [("A", 1), ("C", 2)], "C": [
     ("A", 3), ("B", 2), ("D", 5), ("E", 4)], "D": [("C", 5), ("E", 6)], "E": [("C", 4), ("D", 6)]}
 graph1 = {"A": [("B", 2), ("C", 3), ("D", 8)],
@@ -18,5 +21,5 @@ graph1 = {"A": [("B", 2), ("C", 3), ("D", 8)],
           "D": [("A", 8), ("B", 4), ("C", 6), ("F", 4)],
           "E": [("C", 9), ("F", 3)],
           "F": [("D", 4), ("E", 3)]}
-print(dijktras(graph, 'A'))
-print(dijktras(graph1, 'A'))
+dijktras(graph, 'A')
+dijktras(graph1, 'A')
--- floydwarshal.py
+++ floydwarshal.py
@@ -8,5 +8,12 @@ def fwa(graph,n):
                     graph[i][j] = min(graph[i][j],graph[i][k]+graph[k][j])
     return graph
 
+# n=int(input("Enter the number of nodes: "))
+# graphs=[]
+# for i in range(n):
+#     graphs.append(list(map(int,input().split())))
+
 graph=[[0,1,3,inf,inf],[1,0,2,inf,inf],[3,2,0,5,4],[inf,inf,5,0,6],[inf,inf,4,6,0]]
 print(fwa(graph,5))
+# graph1=[[0,3,inf,7],[8,0,2,inf],[5,inf,0,1],[2,inf,inf,0]]
+# print(fwa(graph1,4))
